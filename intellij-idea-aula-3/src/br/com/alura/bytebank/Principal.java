4da3157db32375f414de1731e4a34d6e0748086e
Da Silva <14689@global.ul.com>
2018-10-08 08:43:25 -0300

    Finalizando o curso de Intellij

--- intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
+++ intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
@@ -1,10 +1,37 @@
 package br.com.alura.bytebank;
 
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.model.Pagamento;
 import br.com.alura.bytebank.visualizador.OpcoesPagamento;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new OpcoesPagamento().mensagemDeBoasVindas();
+
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentos = leitor.ler(1);
+
+        for (Pagamento pagamento: pagamentos) {
+
+            double valorBruto = pagamento.getValor();
+
+            if (valorBruto > 100) {
+                double valorComAcrescimento = valorBruto * 1.1;
+
+                pagamento.setValor(valorComAcrescimento);
+            }
+
+            System.out.println(pagamento);
+
+
+
+        }
+
+
     }
 
 }
+
